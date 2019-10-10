010b27ff43903ce79c939aa2ec8ef8ee522cfe45
Buenomaurilio <mauriliodsb@gmail.com>
2019-10-10 16:59:23 -0300

    frontend  cont.2

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 const app = express();
@@ -9,11 +11,11 @@ mongoose.connect('mongodb+srv://iotontos:iotontos@cluster0-akdvn.mongodb.net/iot
     useUnifiedTopology: true,
 })
 
-
+app.use(cors()); 
 app.use(express.json());
 app.use(routes); 
 
-app.listen(3333); 
+app.listen(3333);  
 
 
 
